a13b25cd08ba87e7d5d2220576f2334ae3b1e530
cwang101 <625250454@qq.com>
2018-04-16 18:58:49 +0800

    InputValidatorTest complete!

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -1,8 +1,34 @@
 package tw.core;
 
+import org.junit.Test;
+import tw.validator.InputValidator;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
 /**
  * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
  */
 public class InputValidatorTest {
+    @Test
+    public void should_return_false_when_input_count_is_not_4() throws Exception {
+        InputValidator validator=new InputValidator();
+        assertFalse(validator.validate("1 2 3"));
+        assertFalse(validator.validate("1 2 3 4 5"));
+    }
 
+    @Test
+    public void should_return_false_when_input_contains_number_grater_than_9() throws Exception {
+        InputValidator validator=new InputValidator();
+        assertFalse(validator.validate("1 2 3 10"));
+        assertFalse(validator.validate("1 2 3 20"));
+    }
+
+    @Test
+    public void should_return_true_when_input_count_is_correct() throws Exception {
+        InputValidator validator=new InputValidator();
+        assertTrue(validator.validate("1 2 3 4"));
+        assertTrue(validator.validate("0 3 2 5"));
+    }
 }
--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,12 @@
 package tw.core.generator;
 
+import org.junit.Test;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
+
 
 }
 
