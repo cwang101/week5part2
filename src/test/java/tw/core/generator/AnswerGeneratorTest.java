ec3af29471fc77d05d58b9a1d179358e10f37ce3
cwang101 <625250454@qq.com>
2018-04-16 20:50:48 +0800

    AnswerGeneratorTest complete!

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,12 +1,26 @@
 package tw.core.generator;
 
 import org.junit.Test;
+import tw.core.Answer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
 
-
+    @Test
+    public void generate_test() throws Exception{
+        RandomIntGenerator mockedIntGenerator=mock(RandomIntGenerator.class);
+        when(mockedIntGenerator.generateNums(9,4)).thenReturn("1 2 3 4");
+        AnswerGenerator generator=new AnswerGenerator(mockedIntGenerator);
+        String expected="1 2 3 4";
+        Answer result = generator.generate();
+        assertEquals(result.toString(),expected);
+    }
 }
 
